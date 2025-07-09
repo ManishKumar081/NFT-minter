c9a98a03b270828d46b16fa5663a7b2892e84789
Pinaka07 <anandsingh.as1996@gmail.com>
2025-07-09 22:29:26 +0530

    add new

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    const NFT = await ethers.getContractFactory("PhysicalProductNFT");
+    const nft = await NFT.deploy();
+  
+    await nft.deployed();
+    console.log("PhysicalProductNFT deployed to:", nft.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
